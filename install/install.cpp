1a3b2b0f4833ca815d9de0df92f759e46e8fd32a
Dyneteve <dyneteve@pixelexperience.org>
2020-06-27 19:36:32 +0200

    recovery: Use ro.build.product to detect the device codename
    * ro.product.device may be changed to fake props for NGA
    
    Change-Id: If0f713ca46ce645a69b1c6edc4bf1d34dc20e74e

--- install/install.cpp
+++ install/install.cpp
@@ -213,7 +213,7 @@ bool CheckPackageMetadata(const std::map<std::string, std::string>& metadata, Ot
     return false;
   }
 
-  auto device = android::base::GetProperty("ro.product.device", "");
+  auto device = android::base::GetProperty("ro.build.product", "");
   auto pkg_device = get_value(metadata, "pre-device");
   // device name can be a | separated list, so need to check
   if (pkg_device.empty() || !isInStringList(device, pkg_device, FINGERPRING_SEPARATOR ":" ",")) {
