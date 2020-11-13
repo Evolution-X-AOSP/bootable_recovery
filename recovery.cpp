04b66abcb29fc90cc08b01e17ece59bc5dafaaac
Kevin F. Haggerty <haggertk@lineageos.org>
2020-11-13 09:54:47 -0700

    Revert "recovery: report compliant reboot reason (Part Deux)"
    This reverts commit 488cc05c9685277bfba148ad259bde10bdc4a439.
    
    * Reason for revert: Booting out of recovery (choose `Reboot system
      now`) on legacy devices is broken. Device keeps booting back into
      recovery.
    * I don't see how this was remotely correct on non-legacy, since there
      wasn't a reboot/shutdown target that was anything other than
      recovery or fastbot.
    
    Change-Id: Ic63ec86297d8b6a88d6075be590c932d814af641

--- recovery.cpp
+++ recovery.cpp
@@ -483,8 +483,6 @@ change_menu:
       case Device::MENU_ADVANCED:
         goto change_menu;
 
-      case Device::REBOOT_FROM_FASTBOOT:    // Can not happen
-      case Device::SHUTDOWN_FROM_FASTBOOT:  // Can not happen
       case Device::NO_ACTION:
         break;
 
