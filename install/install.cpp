7e83ae191e2edfa872c6f8887e937d5b2858eb19
Dyneteve <dyneteve@hentaios.com>
2021-06-09 12:29:32 +0200

    install: Use ro.system_ext.build.fingerprint for incremental
    Change-Id: Ia1d2ffc8bf9dbc69ffcc1a7f046e1ab99acc5d35

--- install/install.cpp
+++ install/install.cpp
@@ -158,7 +158,7 @@ static bool CheckAbSpecificMetadata(const std::map<std::string, std::string>& me
     return false;
   }
 
-  auto device_fingerprint = android::base::GetProperty("ro.build.fingerprint", "");
+  auto device_fingerprint = android::base::GetProperty("ro.system_ext.build.fingerprint", "");
   auto pkg_pre_build_fingerprint = get_value(metadata, "pre-build");
   if (!pkg_pre_build_fingerprint.empty() &&
       !isInStringList(device_fingerprint, pkg_pre_build_fingerprint, FINGERPRING_SEPARATOR)) {
