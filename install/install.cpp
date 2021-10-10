b0ca0451f478376da2cb2ed662a9bb05c7f0ff64
jhenrique09 <jhsv09@gmail.com>
2021-10-10 15:33:05 -0300

    recovery: Disable downgrade check
    Change-Id: I734fad12a54a232a00240ba105a79828135896a9
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- install/install.cpp
+++ install/install.cpp
@@ -176,7 +176,7 @@ static bool CheckAbSpecificMetadata(const std::map<std::string, std::string>& me
 
   // Check for downgrade version.
   bool undeclared_downgrade = false;
-  int64_t build_timestamp =
+  /*int64_t build_timestamp =
       android::base::GetIntProperty("ro.build.date.utc", std::numeric_limits<int64_t>::max());
   int64_t pkg_post_timestamp = 0;
   // We allow to full update to the same version we are running, in case there
@@ -195,7 +195,7 @@ static bool CheckAbSpecificMetadata(const std::map<std::string, std::string>& me
       LOG(ERROR) << "Downgrade package must have a pre-build version set, not allowed.";
       undeclared_downgrade = true;
     }
-  }
+  }*/
 
   if (undeclared_downgrade &&
       !(ui->IsTextVisible() && ask_to_continue_downgrade(ui->GetDevice()))) {
