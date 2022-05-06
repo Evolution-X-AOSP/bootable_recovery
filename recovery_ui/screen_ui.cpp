b983f4f13bcd847bb2c8f25db81105c8a38bc857
Joey Huab <joey@evolution-x.org>
2022-05-06 23:04:27 +0800

    recovery: Update to Evolution X logos
    Co-Authored-By: Akito Mizukito <akito@evolution-x.org>
    Change-Id: Idcd73ee21ac03eefc2e6f07fb259a90c905da231
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- recovery_ui/screen_ui.cpp
+++ recovery_ui/screen_ui.cpp
@@ -558,21 +558,21 @@ void ScreenRecoveryUI::draw_foreground_locked() {
   }
 }
 
-/* pixel blue: #227BF6 */
+/* evolution x blue: #025EFD */
 void ScreenRecoveryUI::SetColor(UIElement e) const {
   switch (e) {
     case UIElement::INFO:
-        gr_color(0x22, 0x7b, 0xf6, 255);
+        gr_color(0x2, 0x5e, 0xfd, 255);
       break;
     case UIElement::HEADER:
-        gr_color(0x22, 0x7b, 0xf6, 255);
+        gr_color(0x2, 0x5e, 0xfd, 255);
       break;
     case UIElement::MENU:
       gr_color(0xd8, 0xd8, 0xd8, 255);
       break;
     case UIElement::MENU_SEL_BG:
     case UIElement::SCROLLBAR:
-        gr_color(0x22, 0x7b, 0xf6, 255);
+        gr_color(0x2, 0x5e, 0xfd, 255);
       break;
     case UIElement::MENU_SEL_BG_ACTIVE:
       gr_color(0, 156, 100, 255);
