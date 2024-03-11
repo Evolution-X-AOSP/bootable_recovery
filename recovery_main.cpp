45c2ee46c1ccd950b2324ed86b3a2caea9ac221b
LuK1337 <priv.luk@gmail.com>
2024-03-11 16:22:34 +0100

    recovery: Update logo after going back to recovery
    Test: Enter fastboot, then enter recovery
    Change-Id: Iee894e584451673bdaee6b2e01afaff2c3eb391f

--- recovery_main.cpp
+++ recovery_main.cpp
@@ -600,6 +600,7 @@ int main(int argc, char** argv) {
       case Device::ENTER_RECOVERY:
         LOG(INFO) << "Entering recovery";
         fastboot = false;
+        ui->SetEnableFastbootdLogo(fastboot);
         device->GoHome();
         break;
 
